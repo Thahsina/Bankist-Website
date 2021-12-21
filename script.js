e2669b45e2cfe5ce4dd3a5247da2b31e6f1d8dcf
Thahsina <s.thahsina@gmail.com>
2021-12-21 21:26:58 +0300

    added more functionality

---
+++ script.js
@@ -0,0 +1,145 @@
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".btn--close-modal");
+const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+const nav = document.querySelector(".nav");
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+
+const openModal = function (e) {
+  e.preventDefault();   
+//   coz btn open account is a link which has href='#' which makes the page go up to its default position which we dont want so e.preventdefault() gives us a smooth look with no effect on home page whenever we click on one of the open account btns.
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+btnsOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
+
+// Button scrolling
+btnScrollTo.addEventListener("click", function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log("Current scroll (X/Y)", window.pageXOffset, window.pageYOffset);
+
+  console.log(
+    "height/width viewport",
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  // Scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  section1.scrollIntoView({ behavior: "smooth" });
+});
+
+
+
+// Tabbed component
+
+tabsContainer.addEventListener('click', function(e){
+    const clicked = e.target.closest('.operations__tab')
+    console.log(clicked);
+    // Gurad clause
+    if (!clicked) return
+
+    tabs.forEach(t => t.classList.remove('operations__tab--active'))
+    clicked.classList.add('operations__tab--active')
+
+    tabsContent.forEach(content => content.classList.remove('operations__content--active'))
+    document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+})
+
+
+const handleHover = function (e) {
+    if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    console.log(link.closest('.nav'));
+    
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    console.log(siblings);
+    //   closest gives us the closest element to nav named 'img'. it gives us all the elements with img or nav__link not just the first one 
+    const logo = link.closest('.nav').querySelector('img');
+
+        siblings.forEach(el => {
+        if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+    }
+};
+
+  // Passing "argument" into handler
+//   
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
+
+// -------------/////////////Sticky Navbar/////////////////////--------//
+
+// const initialCoords = section1.getBoundingClientRect()
+// console.log(initialCoords);
+
+// window.addEventListener('scroll', function(){
+//     if (window.scrollY > initialCoords.top - 200)
+//         nav.classList.add('sticky')
+//     else
+//         nav.classList.remove('sticky')
+// })
+
+
+
+//-------------------- section1.getboundingclinetrect( ) is the distance of section1 from the top position of the webpage.
+// -------------------------window.scrollY  gives us the value of how far has the page scrolled from its initial position.
+
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  // console.log(entry);
+
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+
+headerObserver.observe(header);
+
+
